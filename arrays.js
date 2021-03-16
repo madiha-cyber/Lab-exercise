db5bdaaab1f29a6ac7504e895efc4d5d0d802f86
vagrant <munawaer.ainiwaer@gmail.com>
2021-03-16 19:55:41 +0000

    Complete arrays.js function conversions
    Co-authored-by: Madiha Latif <madihagoheer@outlook.com>

--- arrays.js
+++ arrays.js
@@ -1,21 +1,38 @@
+
 "use strict";
 
 
 // 1. printIndices
 function printIndices(items) {
   for (const i in items){
-    console.log(items[i], ${i})
+    console.log(items[i], i);
   }
 }
+printIndices(['hi', 'hello', 'apple', 'orange'])
+
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const i in items){
+    if (i % 2 === 0){
+     result.push(items[i]); 
+    }
+  }
+  console.log(result);
 }
 
+everyOtherItem(['hi', 'hello', 'apple', 'orange'])
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a,b) => a-b);
+  const sortedNItems = sortedItems.slice(0,n);
+  sortedNItems.reverse();
+  console.log(sortedNItems);
+
 }
+
+smallestNItems([1, 5, 432, 657, 98, 32, 65, 2])
